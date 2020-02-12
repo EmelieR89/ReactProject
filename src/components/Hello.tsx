c50f19f24b00138dc5ccccdfc329a78aecb9a2d8
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2020-02-12 11:24:42 +0100

    init commit

---
+++ src/components/Hello.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export interface HelloProps {
+  compiler: string;
+  framework: string;
+}
+
+export const Hello = (props: HelloProps) => (
+  <h1 style={{ color: "green" }}>
+    Hello from {props.compiler} and {props.framework}!
+  </h1>
+);
